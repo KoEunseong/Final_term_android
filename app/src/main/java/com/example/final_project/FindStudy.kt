6721eb27c12004e45fd5e84e1d0bb3f9672da87e
Eunseong Ko <dmsdl261@naver.com>
2022-12-03 16:20:34 +0900

    임의로 findStudy 페이지를 만들어 인텐트 연결함, 학습 기록 탭에는 join 페이지를 연결해 놨으나 후에 변경예정

---
+++ app/src/main/java/com/example/final_project/FindStudy.kt
@@ -0,0 +1,12 @@
+package com.example.final_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FindStudy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.findstudy)
+
+    }
+}
--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.final_project
 
 import android.app.TabActivity
+import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,17 +12,21 @@ class MainActivity : TabActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var tabHost = this.tabhost
+        var intent : Intent
+
 
         var tabSpecHome = tabHost.newTabSpec("home").setIndicator("홈")
-        tabSpecHome.setContent(R.id.home)
+        tabSpecHome.setContent(R.id.home) //여기서 인텐트 연결 하면 될듯
         tabHost.addTab(tabSpecHome)
 
+        intent = Intent(applicationContext,FindStudy::class.java)
         var tabSpecFind = tabHost.newTabSpec("find").setIndicator("스터디 찾기")
-        tabSpecFind.setContent(R.id.find_study)
+        tabSpecFind.setContent(intent)
         tabHost.addTab(tabSpecFind)
 
+        intent = Intent(applicationContext,join::class.java)
         var tabSpecRecord = tabHost.newTabSpec("record").setIndicator("학습 기록")
-        tabSpecRecord.setContent(R.id.record_study)
+        tabSpecRecord.setContent(intent)
         tabHost.addTab(tabSpecRecord)
 
         var tabSpecSettings = tabHost.newTabSpec("settings").setIndicator("설정")
