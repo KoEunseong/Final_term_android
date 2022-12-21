00c66488aa498f37b75560a4ab5bea6c3d14f89d
DESKTOP-DSBGP8J\82107 <kimdohyeong03@naver.com>
2022-12-22 02:07:35 +0900

    commit

--- app/src/main/java/com/example/final_project/RecordDBHelper.kt
+++ app/src/main/java/com/example/final_project/RecordDBHelper.kt
@@ -8,32 +8,33 @@ import android.database.sqlite.SQLiteOpenHelper
 class RecordDBHelper(context: Context) : SQLiteOpenHelper(context, "recordDB.db", null, 1) {
 
     override fun onCreate(p0: SQLiteDatabase?) {
-        p0!!.execSQL("CREATE TABLE  record_tbl ( memberId INTEGER PRIMARY KEY, date TEXT, memo TEXT);")
+        p0!!.execSQL("CREATE TABLE  recordTbl ( memberId INTEGER PRIMARY KEY, date TEXT, memo TEXT);")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        p0!!.execSQL("DROP TABLE IF EXISTS record_tbl")
+        p0!!.execSQL("DROP TABLE IF EXISTS recordTbl")
         onCreate(p0)
     }
-    fun insertData(memberId : Int?, date : String? , memo : String) : Boolean {
+    fun insertData(memberId : Int?, date : String? , memo : String) : Unit {
         val myDB = this.writableDatabase
-        val contentValues = ContentValues()
-        contentValues.put("memberId",memberId)
-        contentValues.put("date",date)
-        contentValues.put("memo",memo)
-        val result = myDB.insert("record_tbl",null,contentValues)
 
-        if(result == -1L)
-            return false
-        return true
+        val values = ContentValues()
+        values.put("memberId", memberId)
+        values.put("date", date)
+        values.put("memo", memo)
+
+        myDB.insert("recordTbl", null, values)
+
     }
 
     fun selectMemoAndDate(memberId: Int,date:String) : String {
         val myDB = this.writableDatabase
-        var res = true
-        val cursor = myDB.rawQuery("select * from record_tbl where email = ? AND memberId", null)
-        var memo = cursor.getString(2)
-        return memo
+        var sql = "select * from recordTbl where memberId = "+memberId+" AND date = "+date
 
+        val cursor = myDB.rawQuery(sql, null)
+        var memo = cursor.getString(2)
+
+
+        return memo
     }
 }
--- app/src/main/java/com/example/final_project/Study_record.kt
+++ app/src/main/java/com/example/final_project/Study_record.kt
@@ -1,6 +1,7 @@
 package com.example.final_project
 
 import android.app.Activity
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.widget.*
 
@@ -8,6 +9,9 @@ class Study_record :Activity() {
 
     lateinit var calendarview :CalendarView
     lateinit var recordView : LinearLayout
+
+    lateinit var myHelper: RecordDBHelper
+    lateinit var sqlDB: SQLiteDatabase
 
     lateinit var insertBtn : Button
     lateinit var editTv : EditText
@@ -28,23 +32,24 @@ class Study_record :Activity() {
 
         calendarview.setOnDateChangeListener { calendarView, i, i2, i3 ->
             year = i.toString()
-            month= i2.toString()
+            month= (i2+1).toString()
             dayOfMonth = i3.toString()
 
-            Toast.makeText(this, year + "년 " + (month + 1) + "월 " + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, year + "년 " + month  + "월 " + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
 
             date1 = year+"-"+month+"-"+dayOfMonth
 
             // db에서 날짜별 메모 조회
-            memo = selectMemo
+            memo = myHelper!!.selectMemoAndDate(1,date1)
             editTv.setText(memo)
 
+
+            myHelper.close()
         }
 
         insertBtn = findViewById<Button>(R.id.insetBtn)
 
         insertBtn.setOnClickListener {
-
             editTv = findViewById<EditText>(R.id.ed1Text)
             memo = selectMemo + editTv.text.toString()
 
@@ -53,6 +58,10 @@ class Study_record :Activity() {
             // db에 INSERT
 
 
+            Toast.makeText(this,"메모가 추가되었습니다",Toast.LENGTH_SHORT).show()
+
+            myHelper!!.insertData(1,"2022-12-22",memo)
+            myHelper.close()
         }
     }
 }
